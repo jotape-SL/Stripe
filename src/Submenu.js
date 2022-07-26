c64a3bae6cfbb7606512d6f45946eec43db4e307
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-26 10:44:26 -0300

    submenu display, dynamic length

--- src/Submenu.js
+++ src/Submenu.js
@@ -9,18 +9,38 @@ const Submenu = () => {
     page: { page, links },
   } = useGlobalContext();
   const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
   useEffect(() => {
+    setColumns('col-2');
     const subMenu = container.current;
     const { center, bottom } = location;
     subMenu.style.left = `${center}px`;
     subMenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+    if (links.length > 3) {
+      setColumns('col-4');
+    }
+  }, [location, links]);
   return (
     <aside
       className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
       ref={container}
     >
       <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={index}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
