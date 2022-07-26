4a5a00e838b40619819652c99fa4e17dea4ddd61
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-26 10:31:25 -0300

    selected the buttons

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,7 +1,28 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaCentercode } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+  const container = useRef(null);
+  useEffect(() => {
+    const subMenu = container.current;
+    const { center, bottom } = location;
+    subMenu.style.left = `${center}px`;
+    subMenu.style.top = `${bottom}px`;
+  }, [location]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+    </aside>
+  );
+};
 
-export default Submenu
+export default Submenu;
